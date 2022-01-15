f7344566246ea92127d838fc0dadc899767eb320
Robson Pereira <robson.ads.fatec@gmail.com>
2022-01-15 17:56:03 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,8 +1,14 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard() {
+type Props = {
+    movie:Movie;
+}
 
+function MovieCard( { movie } : Props) {
+
+    /*
     const movie = {
         id: 1,
         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
@@ -10,7 +16,8 @@ function MovieCard() {
         count: 2,
         score: 4.5
     };
-
+    */ 
+   
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,15 +8,25 @@ import { BASE_URL } from "utils/requests";
 function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)//erro no console???
-        .then(response => {
-            const data = response.data as MoviePage;
-            console.log(data);
-            setPageNumber(data.number); 
-        });
-    }, []);
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)//erro no console???
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
 
     /*
     //erro no console???
@@ -26,31 +36,28 @@ function Listing() {
             setPageNumber(data.number); //não carregou o número da página na "tela"
         });
     */
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
     return (
         <>
-            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie =>
+                    (//erro, não listou nada!!!
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </>
